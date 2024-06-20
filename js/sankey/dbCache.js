7e4f02b53c886de951e369db49630ee496b3893d
marcio <78marcio@gmail.com>
2024-06-20 22:51:43 +0200

    clean up code

--- js/sankey/dbCache.js
+++ js/sankey/dbCache.js
@@ -1,35 +1,37 @@
-	const cacheName = "estat-dataset4";
-function fetchDataset(query) {
-	const url = EuroJSONstat.getURL(query);
-	// cache the response
-	return caches.open(cacheName).then((cache) => {
-		return cache.match(url).then((response) => {
-			if (response) {
-				return response.json();
-			} else {
-				return fetch(url).then((response) => {
-					cache.put(url, response.clone());
-          return response.json();
-				});
-			}
-		});
-	});
+const cacheName = "estat-dataset4";
+
+async function fetchDataset(query) {
+  const url = EuroJSONstat.getURL(query);
+
+  const cache = await caches.open(cacheName);
+  const cachedResponse = await cache.match(url);
+
+  if (cachedResponse) {
+    return cachedResponse.json();
+  }
+
+  const networkResponse = await fetch(url);
+  cache.put(url, networkResponse.clone());
+  return networkResponse.json();
 }
 
-function getEUDS() {
-	let resp = [];
-  ["nrg_bal_c"].forEach(function (dsName) {
-		const query = queryBuilder(dsName, { flow: "FC_E" });
-		const apiResponse = fetchDataset(query);
-    console.log("apiResponse", apiResponse);
-		apiResponse.then((res) => {
-			console.log("res", res);
-			// const data =
-			// const parsedDs = nsDbq.parsePromise(res);
-			// console.log("parsedDs", parsedDs);
-			// resp.push(parsedDs);
+async function getEUDS() {
+  const resp = [];
+  const datasets = ["nrg_bal_c"];
+
+  for (const dsName of datasets) {
+    const query = queryBuilder(dsName, { flow: "FC_E" });
+    try {
+      const res = await fetchDataset(query);
+      console.log("res", res);
+      // const parsedDs = nsDbq.parsePromise(res);
+      // console.log("parsedDs", parsedDs);
+      // resp.push(parsedDs);
       resp.push(res);
-		});
-	});
+    } catch (error) {
+      console.error(`Failed to fetch dataset ${dsName}:`, error);
+    }
+  }
+
   return resp;
 }
--- js/sankey/dbq.js
+++ js/sankey/dbq.js
@@ -1,158 +1,159 @@
 const nsDbq = {
-	Promises: function () {
-		// const datasetNames = ["nrg_bal_c", "nrg_bal_sd"];
-		const datasetNames = ["nrg_bal_c"];
-		// const datasetNames = ["nrg_bal_c"];
-		let dsPromises = [];
-
-		datasetNames.forEach(function (datasetName) {
-			const queryFilter = {
-				// time: "2018",
-				// geo: "AT",
-				flow: "all",
-				},
-				query = queryBuilder(datasetName, queryFilter);
-			dataset = EuroJSONstat.fetchDataset(query).then((data) => {
-				return nsDbq.parsePromise(data);
-			});
-
-			dsPromises.push(dataset);
-		});
-
-		return dsPromises;
+	async Promises() {
+	  const datasetNames = ["nrg_bal_c"];
+	  const dsPromises = datasetNames.map(async (datasetName) => {
+		const queryFilter = { flow: "all" };
+		const query = queryBuilder(datasetName, queryFilter);
+		const data = await fetchDataset(query);
+		return this.parsePromise(data);
+	  });
+  
+	  return Promise.all(dsPromises);
 	},
-
-parsePromise: function (data) {
-	console.log(data);
-	const sankeyDBnew = {
+  
+	parsePromise(data) {
+	  const dimensions = ["geo", "nrg_bal", "siec", "time"];
+	  const sankeyDBnew = {
 		value: data.value,
 		mask: data.value.map(() => true),
 		dimension: {
-			id: ["geo", "flow", "fuel", "year"],
-			size: [data.Dimension("geo").length, data.Dimension("nrg_bal").length, data.Dimension("siec").length, data.Dimension("time").length],
-			geo: {
-				index: data.Dimension("geo").id,
-				label: {},
-			},
-			flow: {
-				index: data.Dimension("nrg_bal").id,
-				label: {},
-			},
-			fuel: {
-				index: data.Dimension("siec").id,
-				label: {},
-			},
-			year: {
-				index: data.Dimension("time").id,
-				label: {},
-			},
+		  id: ["geo", "flow", "fuel", "year"],
+		  size: dimensions.map(dim => data.Dimension(dim).length),
+		  ...dimensions.reduce((acc, dim) => {
+			acc[dim === "nrg_bal" ? "flow" : dim] = {
+			  index: data.Dimension(dim).id,
+			  label: {},
+			};
+			return acc;
+		  }, {}),
 		},
-	};
-	return sankeyDBnew;
-},
-
-	renderChart: function () {
-	// showCodesFor_c();
-
-		// const promises = nsDbq.Promises();
-		const promises = getEUDS();
-
-		navigator.storage.estimate().then((estimate) => {
-			document.getElementById("percent").value = (
-				(estimate.usage / estimate.quota) *
-				100
-			).toFixed(2);
-		});
-
-		caches.keys().then((keys) => {
-			keys.map((key) => {
-				console.log(key);
-				caches.delete(key);
-			});
-		});
-		Promise.all(promises)
-			.then(function (data) {
-				console.log("data", data);
-				console.log("data0", data[0].dimension.size);
-				// console.log("data1", data[1].dimension.size);
-				
-				// var nrg_bal_c= new Set(data[0].dimension.flow.index);
-				// var nrg_bal_sd= data[1].dimension.flow.index;
-				// var extraInSd = nrg_bal_sd.filter(x => !nrg_bal_c.has(x))
-				// console.log("extraInSd", extraInSd);
-				// var nrg_bal_sd= new Set(data[1].dimension.flow.index);
-				// var nrg_bal_c= data[0].dimension.flow.index;
-				// var extraInC = nrg_bal_c.filter(x => !nrg_bal_sd.has(x))
-				// console.log("extraInC", extraInC);
-
-				// data.forEach(function (dataset) {
-				// console.log("dataset", dataset);	
-				// });
-			});
+	  };
+	  return sankeyDBnew;
 	},
-};
-
-
-function queryBuilder( datasetName, f) {
-
-	f.fuel = f.fuel || ["TOTAL"];
-	f.flow = f.flow || ["FC_E"];
-	f.unit = f.unit || ["KTOE"];
-
-  let query = {
-    dataset: datasetName,
-    lang: "de",
-    filter: {
-      nrg_bal: f.flow,
-      geo: f.geo,
-      siec: f.fuel,
-      unit: f.unit,
-      time: f.time,
-    },
+  
+	async renderChart() {
+	  await this.clearCache();
+  
+	  navigator.storage.estimate().then((estimate) => {
+		document.getElementById("percent").value = (
+		  (estimate.usage / estimate.quota) *
+		  100
+		).toFixed(2);
+	  });
+  
+	  try {
+		const data = await this.Promises();
+		console.log("data", data);
+		console.log("data0", data[0].dimension.size);
+	  } catch (error) {
+		console.error("Error fetching data:", error);
+	  }
+	},
+  
+	async clearCache() {
+	  const keys = await caches.keys();
+	  await Promise.all(keys.map((key) => caches.delete(key)));
+	},
   };
 
-	if (!f.geo || f.geo === "all") {
-		query = EuroJSONstat.removeParamQuery(query, ["geo"]);
+  function queryBuilder(datasetName, filters = {}) {
+	const defaultFilters = {
+	  fuel: ["TOTAL"],
+	  flow: ["FC_E"],
+	  unit: ["KTOE"],
+	};
+  
+	const queryFilters = { ...defaultFilters, ...filters };
+  
+	let query = {
+	  dataset: datasetName,
+	  lang: "de",
+	  filter: {
+		nrg_bal: queryFilters.flow,
+		geo: queryFilters.geo,
+		siec: queryFilters.fuel,
+		unit: queryFilters.unit,
+		time: queryFilters.time,
+	  },
+	};
+  
+	if (!queryFilters.geo || queryFilters.geo === "all") {
+	  query = EuroJSONstat.removeParamQuery(query, ["geo"]);
 	}
-
-	if (!f.flow || f.flow === "all") {
-		query = EuroJSONstat.removeParamQuery(query, ["nrg_bal"]);
+  
+	if (!queryFilters.flow || queryFilters.flow === "all") {
+	  query = EuroJSONstat.removeParamQuery(query, ["nrg_bal"]);
 	}
-
-  if (!f.time || f.time === "all") {
-    query = EuroJSONstat.removeParamQuery(query, ["time"]);
-    query = EuroJSONstat.addParamQuery(query, { sinceTimePeriod: 1990 });
+  
+	if (!queryFilters.time || queryFilters.time === "all") {
+	  query = EuroJSONstat.removeParamQuery(query, ["time"]);
+	  query = EuroJSONstat.addParamQuery(query, { sinceTimePeriod: 1990 });
+	}
+  
+	if (queryFilters.timeSeries === "latest") {
+	  query = EuroJSONstat.lastPeriodQuery(query);
+	}
+  
+	console.log("query url:", EuroJSONstat.getURL(query));
+  
+	return query;
   }
 
-  if (f.timeSeries === "latest") {
-    query = EuroJSONstat.lastPeriodQuery(query);
-  }
-
-	1 && console.log("query url:", EuroJSONstat.getURL(query));
-
-  return query;
-}
-
-function showCodesFor_c() {
-	var codes_NRG_BAL_C = {
-		flow: [
-			"AAS", "PRD", "PPRD", "IMP", "FC_E", "FC_NE", "EXP", "INTMARB", "NRG_E", "DL", "INTAVI", "TI_NRG_FC_IND_NE", "FC_TRA_NE", "FC_OTH_NE", "FC_IND_E", "FC_IND_IS_E", "FC_IND_CPC_E", "FC_IND_NFM_E", "FC_IND_NMM_E", "FC_IND_TE_E", "FC_IND_MAC_E", "FC_IND_MQ_E", "FC_IND_FBT_E", "FC_IND_PPP_E", "FC_IND_WP_E", "FC_IND_CON_E", "FC_IND_TL_E", "FC_IND_NSP_E", "FC_TRA_E", "FC_TRA_RAIL_E", "FC_TRA_ROAD_E", "FC_TRA_DAVI_E", "FC_TRA_DNAVI_E", "FC_TRA_PIPE_E", "FC_TRA_NSP_E", "FC_OTH_E", "FC_OTH_CP_E", "FC_OTH_HH_E", "FC_OTH_AF_E", "FC_OTH_FISH_E", "FC_OTH_NSP_E", "RCV_RCY", "STK_CHG", "STATDIFF", "TI_E", "TO", "STK_BLD", "AFC", "TO_EHG_MAPCHP", "TI_RPI_E", "TO_RPI", "TI_BKBPB_E", "TO_BKBPB", "TI_CO_E", "TO_CO", "TI_BF_E", "TO_BF", "TI_GW_E", "TO_GW", "TI_EHG_E", "TO_EHG", "TI_OTH", "TI_CL_E", "TI_BNG_E", "TI_LBB_E", "TI_CPP_E", "TI_GTL_E", "TI_NSP_E", "TO_OTH", "TO_CL", "TO_BNG", "TO_LBB", "TO_CPP", "TO_GTL", "TO_NSP",
-			//Refinery disaggregation flows
-			"TI_RPI_RI_E", "TO_RPI_RO", "TI_RPI_PII_E", "TO_RPI_PIR", "TI_RPI_OTH_E", "TI_RPI_BPI_E", "TI_RPI_PT_E", "TI_RPI_IT_E", "TI_RPI_DU_E", "TO_RPI_O_OTH", "TO_RPI_PPR", "TO_RPI_PT", "TO_RPI_BKFLOW", "TO_RPI_IT",
-			//Electricity and heat generation disaggregation flows
-			"TI_EHG_EONL_E", "TI_EHG_MAPE_E", "TI_EHG_APE_E", "TO_EHG_EONL", "TO_EHG_MAPE", "TO_EHG_APE", "TI_EHG_CHP_E", "TI_EHG_MAPCHP_E", "TI_EHG_APCHP_E", "TO_EHG_CHP", "TO_EHG_MAPCHP", "TO_EHG_APCHP", "TI_EHG_HONL_E", "TI_EHG_MAPH_E", "TI_EHG_APH_E", "TO_EHG_HONL", "TO_EHG_MAPH", "TO_EHG_APH", "TI_EHG_OTH", "TI_EHG_EDHP", "TI_EHG_EB", "TI_EHG_EPS", "TI_EHG_DHEP", "TO_EHG_G_OTH", "TO_EHG_EDHP", "TO_EHG_EB", "TO_EHG_PH", "TO_EHG_OTH",
-			//Energy branch disaggregation flows
-			"NRG_EHG_E", "NRG_CM_E", "NRG_OIL_NG_E", "NRG_PF_E", "NRG_CO_E", "NRG_BKBPB_E", "NRG_GW_E", "NRG_BF_E", "NRG_PR_E", "NRG_NI_E", "NRG_CL_E", "NRG_LNG_E", "NRG_BIOG_E", "NRG_GTL_E", "NRG_CPP_E", "NRG_NSP_E",
-			//Transformation losses disaggregation flows
-			"TL_RPI", "TL_CO", "TL_BF", "TL_GW", "TL_BKBPB", "TL_OTH", "TL_CL", "TL_BNG", "TL_LBB", "TL_CPP", "TL_GTL", "TL_NSP",
-			//Refinery Transformation losses disaggregation flows
-			"TL_RPI_RO", "TL_RPI_PIR", "TL_RPI_L_OTH", "TL_RPI_BKFLOW", "TL_RPI_PT", "TL_RPI_IT", "TL_RPI_PPR",
-			//Electricity and heat generation Transformation losses disaggregation flows
-			"TL_EHG_EONL", "TL_EHG_MAPE", "TL_EHG_APE", "TL_EHG_CHP", "TL_EHG_MAPCHP", "TL_EHG_APCHP", "TL_EHG_HONL", "TL_EHG_MAPH", "TL_EHG_APH", "TL_EHG_L_OTH", "TL_EHG_EDHP", "TL_EHG_EB", "TL_EHG_PH", "TL_EHG_EPS_PURE", "TL_EHG_EPS_MIX", "TL_EHG_OTH",
-		],
-		fuel: [
-			"SFF_P1000", "C0121", "C0129", "C0220", "C0311", "SFF_OTH", "O4000", "O4100_TOT", "O4200-4500", "O4652XR5210B", "O4661XR5230B", "O4671XR5220B", "O4680", "PP_OTH", "G3000_C0350-370", "G3000", "C0350-0370", "RA100", "RA300", "RA410", "RA420", "R5110-5150_W6000RI", "R5200", "RA500_5160", "W6100_6220", "W6100", "W6220", "W6210", "RA200", "R5300", "RA600", "N900H", "H8000", "E7000", "C0110", "C0210", "C0320", "C0312", "C0340", "C0330", "P1100", "P1200", "S2000", "O4200", "O4300", "O4400X4410", "O4500", "O4610", "O4620", "O4630", "O4651", "O4653", "O4669", "O4640", "O4691", "O4692", "O4695", "O4694", "O4693", "O4699", "C0360", "C0350", "C0371", "C0379", "R5210P", "R5210B", "R5220P", "R5220B", "R5230P", "R5230B", "R5290", "RA500", "R5160",
-		],
+  
+  function showCodesFor_c() {
+	const codes_NRG_BAL_C = {
+	  flow: [
+		"AAS", "PRD", "PPRD", "IMP", "FC_E", "FC_NE", "EXP", "INTMARB", "NRG_E", "DL", 
+		"INTAVI", "TI_NRG_FC_IND_NE", "FC_TRA_NE", "FC_OTH_NE", "FC_IND_E", "FC_IND_IS_E", 
+		"FC_IND_CPC_E", "FC_IND_NFM_E", "FC_IND_NMM_E", "FC_IND_TE_E", "FC_IND_MAC_E", 
+		"FC_IND_MQ_E", "FC_IND_FBT_E", "FC_IND_PPP_E", "FC_IND_WP_E", "FC_IND_CON_E", 
+		"FC_IND_TL_E", "FC_IND_NSP_E", "FC_TRA_E", "FC_TRA_RAIL_E", "FC_TRA_ROAD_E", 
+		"FC_TRA_DAVI_E", "FC_TRA_DNAVI_E", "FC_TRA_PIPE_E", "FC_TRA_NSP_E", "FC_OTH_E", 
+		"FC_OTH_CP_E", "FC_OTH_HH_E", "FC_OTH_AF_E", "FC_OTH_FISH_E", "FC_OTH_NSP_E", 
+		"RCV_RCY", "STK_CHG", "STATDIFF", "TI_E", "TO", "STK_BLD", "AFC", "TO_EHG_MAPCHP", 
+		"TI_RPI_E", "TO_RPI", "TI_BKBPB_E", "TO_BKBPB", "TI_CO_E", "TO_CO", "TI_BF_E", 
+		"TO_BF", "TI_GW_E", "TO_GW", "TI_EHG_E", "TO_EHG", "TI_OTH", "TI_CL_E", "TI_BNG_E", 
+		"TI_LBB_E", "TI_CPP_E", "TI_GTL_E", "TI_NSP_E", "TO_OTH", "TO_CL", "TO_BNG", 
+		"TO_LBB", "TO_CPP", "TO_GTL", "TO_NSP",
+		// Refinery disaggregation flows
+		"TI_RPI_RI_E", "TO_RPI_RO", "TI_RPI_PII_E", "TO_RPI_PIR", "TI_RPI_OTH_E", "TI_RPI_BPI_E", 
+		"TI_RPI_PT_E", "TI_RPI_IT_E", "TI_RPI_DU_E", "TO_RPI_O_OTH", "TO_RPI_PPR", "TO_RPI_PT", 
+		"TO_RPI_BKFLOW", "TO_RPI_IT",
+		// Electricity and heat generation disaggregation flows
+		"TI_EHG_EONL_E", "TI_EHG_MAPE_E", "TI_EHG_APE_E", "TO_EHG_EONL", "TO_EHG_MAPE", 
+		"TO_EHG_APE", "TI_EHG_CHP_E", "TI_EHG_MAPCHP_E", "TI_EHG_APCHP_E", "TO_EHG_CHP", 
+		"TO_EHG_MAPCHP", "TO_EHG_APCHP", "TI_EHG_HONL_E", "TI_EHG_MAPH_E", "TI_EHG_APH_E", 
+		"TO_EHG_HONL", "TO_EHG_MAPH", "TO_EHG_APH", "TI_EHG_OTH", "TI_EHG_EDHP", "TI_EHG_EB", 
+		"TI_EHG_EPS", "TI_EHG_DHEP", "TO_EHG_G_OTH", "TO_EHG_EDHP", "TO_EHG_EB", "TO_EHG_PH", 
+		"TO_EHG_OTH",
+		// Energy branch disaggregation flows
+		"NRG_EHG_E", "NRG_CM_E", "NRG_OIL_NG_E", "NRG_PF_E", "NRG_CO_E", "NRG_BKBPB_E", 
+		"NRG_GW_E", "NRG_BF_E", "NRG_PR_E", "NRG_NI_E", "NRG_CL_E", "NRG_LNG_E", "NRG_BIOG_E", 
+		"NRG_GTL_E", "NRG_CPP_E", "NRG_NSP_E",
+		// Transformation losses disaggregation flows
+		"TL_RPI", "TL_CO", "TL_BF", "TL_GW", "TL_BKBPB", "TL_OTH", "TL_CL", "TL_BNG", "TL_LBB", 
+		"TL_CPP", "TL_GTL", "TL_NSP",
+		// Refinery Transformation losses disaggregation flows
+		"TL_RPI_RO", "TL_RPI_PIR", "TL_RPI_L_OTH", "TL_RPI_BKFLOW", "TL_RPI_PT", "TL_RPI_IT", 
+		"TL_RPI_PPR",
+		// Electricity and heat generation Transformation losses disaggregation flows
+		"TL_EHG_EONL", "TL_EHG_MAPE", "TL_EHG_APE", "TL_EHG_CHP", "TL_EHG_MAPCHP", "TL_EHG_APCHP", 
+		"TL_EHG_HONL", "TL_EHG_MAPH", "TL_EHG_APH", "TL_EHG_L_OTH", "TL_EHG_EDHP", "TL_EHG_EB", 
+		"TL_EHG_PH", "TL_EHG_EPS_PURE", "TL_EHG_EPS_MIX", "TL_EHG_OTH",
+	  ],
+	  fuel: [
+		"SFF_P1000", "C0121", "C0129", "C0220", "C0311", "SFF_OTH", "O4000", "O4100_TOT", 
+		"O4200-4500", "O4652XR5210B", "O4661XR5230B", "O4671XR5220B", "O4680", "PP_OTH", 
+		"G3000_C0350-370", "G3000", "C0350-0370", "RA100", "RA300", "RA410", "RA420", 
+		"R5110-5150_W6000RI", "R5200", "RA500_5160", "W6100_6220", "W6100", "W6220", 
+		"W6210", "RA200", "R5300", "RA600", "N900H", "H8000", "E7000", "C0110", "C0210", 
+		"C0320", "C0312", "C0340", "C0330", "P1100", "P1200", "S2000", "O4200", "O4300", 
+		"O4400X4410", "O4500", "O4610", "O4620", "O4630", "O4651", "O4653", "O4669", 
+		"O4640", "O4691", "O4692", "O4695", "O4694", "O4693", "O4699", "C0360", "C0350", 
+		"C0371", "C0379", "R5210P", "R5210B", "R5220P", "R5220B", "R5230P", "R5230B", 
+		"R5290", "RA500", "R5160",
+	  ],
 	};
 	console.log(codes_NRG_BAL_C);
-}
+  }
+  
