70d8a06350c52bccbdc480d317f700aaac33e2f6
marcio <78marcio@gmail.com>
2025-02-04 11:13:37 +0100

    fixes

--- js/domComponents/footerComponent.js
+++ js/domComponents/footerComponent.js
@@ -1,74 +1,57 @@
 class Footer {
-    constructor() {
-      this.footer = document.createElement('footer');
-      this.footer.classList.add('pt-4');
-  
-      const footerContainer = document.createElement('div');
-      footerContainer.id = 'footer';
-      footerContainer.classList.add('d-flex', 'flex-wrap', 'justify-content-end', 'align-items-center', 'fixed-bottom', 'py-1');
-  
-      const footerCreditsList = document.createElement('ul');
-      footerCreditsList.id = 'footerCredits';
-      footerCreditsList.classList.add('nav', 'justify-content-end');
-  
-      footerContainer.appendChild(footerCreditsList);
-      this.footer.appendChild(footerContainer);
-    }
+  constructor() {
+    this.footer = document.createElement("footer");
 
+    const footerContainer = document.createElement("div");
+    footerContainer.id = "footer";
+    footerContainer.classList.add("ecl-site-footer");
 
+    const footerCreditsList = document.createElement("ul");
+    footerCreditsList.id = "footerCredits";
+    footerCreditsList.classList.add("ecl-site-footer__list");
 
-/**
- * Builds the links for the footer.
- */
-  
-    buildLinksFooter() {
-      const footerCredits = document.querySelector('#footerCredits');
-      footerCredits.innerHTML = '';
-  
-      const linksContent = ` <li>
-      <a href="https://ec.europa.eu/info/cookies_${REF.language.toLowerCase()}" target="_blank" rel="noreferrer noopener" id="footer-cookies" class="footer-decoration">
-        <span>${languageNameSpace.labels["FOOTER_COOKIES"]}</span>
-        <svg viewBox="0 0 13 20">
-          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
-        </svg>
+    footerContainer.appendChild(footerCreditsList);
+    this.footer.appendChild(footerContainer);
+  }
+
+  /**
+   * Builds the links for the footer.
+   */
+
+  buildLinksFooter() {
+    const footerCredits = document.querySelector("#footerCredits");
+    footerCredits.innerHTML = "";
+
+    const linksContent = /*html*/ `     
+    <li class="ecl-site-footer__list-item">
+    <a id="footer-privacy" href="https://ec.europa.eu/info/legal-notice_${REF.language.toLowerCase()}" title="${languageNameSpace.labels["FOOTER_LEGAL"]}" class="ecl-link ecl-link--standalone ecl-site-footer__link">
+    ${languageNameSpace.labels["FOOTER_LEGAL"]}
+    </a>
+    </li>
+    <li class="ecl-site-footer__list-item">
+    <a id="footer-legal" href="https://ec.europa.eu/info/privacy-policy_${REF.language.toLowerCase()}" title="${languageNameSpace.labels["FOOTER_PRIVACY"]}" class="ecl-link ecl-link--standalone ecl-site-footer__link">
+    ${languageNameSpace.labels["FOOTER_PRIVACY"]}
+    </a>
+    </li>
+    <li class="ecl-site-footer__list-item">
+      <a id="footer-cookies" href="https://ec.europa.eu/info/cookies_${REF.language.toLowerCase()}" title="${languageNameSpace.labels["FOOTER_COOKIES"]}" class="ecl-link ecl-link--standalone ecl-site-footer__link">
+      ${languageNameSpace.labels["FOOTER_COOKIES"]}
       </a>
     </li>
-    <li>
-      <a href="https://ec.europa.eu/info/privacy-policy_${REF.language.toLowerCase()}" target="_blank" rel="noreferrer noopener" id="footer-privacy" class="footer-decoration">
-        <span>${languageNameSpace.labels["FOOTER_PRIVACY"]}</span>
-        <svg viewBox="0 0 13 20">
-          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
-        </svg>
-      </a>
-    </li>
-    <li>
-      <a href="https://ec.europa.eu/info/legal-notice_${REF.language.toLowerCase()}" target="_blank" rel="noreferrer noopener" id="footer-legal" class="footer-decoration">
-        <span>${languageNameSpace.labels["FOOTER_LEGAL"]}</span>
-        <svg viewBox="0 0 13 20">
-          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
-        </svg>
-      </a>
-    </li>
-    <li>
- <a href="https://ec.europa.eu/eurostat/web/main/help/accessibility" target="_blank" rel="noreferrer noopener" id="footer-accessibility" class="footer-decoration">
-        <span>${languageNameSpace.labels["FOOTER_ACCESSIBILITY"]}</span>
-        <svg viewBox="0 0 13 20">
-          <polyline points="0.5 19.5 3 19.5 12.5 10 3 0.5" />
-        </svg>
+    <li class="ecl-site-footer__list-item">
+        <a id="footer-access" href="https://ec.europa.eu/eurostat/web/main/help/accessibility" title="${languageNameSpace.labels["FOOTER_ACCESSIBILITY"]}" class="ecl-link ecl-link--standalone ecl-site-footer__link">
+      ${languageNameSpace.labels["FOOTER_ACCESSIBILITY"]}
       </a>
     </li>`;
-  
-      footerCredits.innerHTML = linksContent;
-    }
-  
-    addToDOM(targetElement) {
-      const container = document.querySelector(targetElement);
-      const mainElement = document.querySelector('main');
-    const parentElement = mainElement.parentNode;
-    parentElement.insertBefore(this.footer, mainElement.nextSibling);
+
+    footerCredits.innerHTML = linksContent;
+  }
+
+  addToDOM(targetElement) {
+    const container = document.querySelector(targetElement);
+    container.appendChild(this.footer);
 
     // Call the buildLinksFooter method after inserting the footer into the DOM
     this.buildLinksFooter();
   }
 }
-  
